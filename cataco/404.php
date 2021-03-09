66a1fea7547a5a21b1fabf842bd05e5094cdf858
WilliamPomroy <williampomroy03@gmail.com>
2021-03-09 16:50:01 -0700

    404 Styles and Layout
    Quickly doing the styles and the layout. There were a few extra bits and bobs and some of the classes had to be changed.

--- cataco/404.php
+++ cataco/404.php
@@ -1,20 +1,14 @@
 <?php get_header(); ?>
 <main id="content">
 	<article id="post-0" class="post not-found">
-	<header class="header">
-	<h1 class="entry-title"><?php esc_html_e( 'Oops!', 'cataco' ); ?></h1>
-	</header>
-	<div class="entry-content">
+	<div class="heading-background">
+        <h2><?php esc_html_e( 'Oops!', 'cataco' ); ?></h2>
+    </div>
+	<div class="error-content">
 		<p><?php esc_html_e( 'The page you are looking for cannot be found.', 'cataco' ); ?></p>
-		<a href="http://lettersbycata.web.dmitcapstone.ca/lettersbycata/">Back to Home</a>
+		<a class="btn" href="<?php echo esc_url( home_url( '/' ) ); ?>">Back to Home</a>
 	</div>
 	</article>
-	<div class="mailing">
-    <h2>Mailing List Sign Up</h2>
-
-    <p>Want to get in on the shop updates, deals, and freebies?</p>
-    <a href="" id="signup">Sign Up Here</a>
-</div>
 </main>
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
