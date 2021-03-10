4823406a0655c594c8d53960b2e9098bf109a58b
WilliamPomroy <williampomroy03@gmail.com>
2021-03-09 23:14:10 -0700

    Footer
    Some small layout changes to the Mailing area of the footer and a wee bit of styling.

--- cataco/footer.php
+++ cataco/footer.php
@@ -1,12 +1,13 @@
 			</div>
 			<div class="mailing">
-				<h2>Mailing List Sign Up</h2>
+				<div class="heading-background">
+					<h2>Mailing List Sign Up</h2>
+				</div>
 
 				<p>Want to get in on the shop updates, deals, and freebies?</p>
-				<a href="<?php echo esc_url( home_url( '/' ) ); ?>/contact" id="signup">Sign Up Here</a>
+				<a class="btn" href="<?php echo esc_url( home_url( '/' ) ); ?>/contact" id="signup">Sign Up Here</a>
 			</div>
 			<footer id="footer">
-				
 					<?php dynamic_sidebar( 'footer_area_top' ); ?>
 				<div class="over-bottom">	
 					<?php dynamic_sidebar( 'footer_area_bot' ); ?>
