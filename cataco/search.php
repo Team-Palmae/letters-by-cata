361eeb8aab0a111498f0b3cc88e345e3c539fb21
WilliamPomroy <williampomroy03@gmail.com>
2021-03-18 18:37:24 -0600

    Search.php header issue
    Fixed a small issue with search.php having the header background and the spacing of it.

--- cataco/search.php
+++ cataco/search.php
@@ -1,14 +1,14 @@
 <?php get_header(); ?>
 <main id="content">
 	<?php if ( have_posts() ) : ?>
-		<header class="header">
-			<h1 class="entry-title"><?php printf( esc_html__( 'Search Results for: %s', 'cataco' ), get_search_query() ); ?></h1>
-		</header>
+		<div class="search-heading">
+			<h2 class="entry-title"><?php printf( esc_html__( 'Search Results for: %s', 'cataco' ), get_search_query() ); ?></h2>
+		</div>
 		<?php while ( have_posts() ) : the_post(); ?>
 			<?php get_template_part( 'entry' ); ?>
 		<?php endwhile; ?>
 		<?php get_template_part( 'nav', 'below' ); ?>
-		<?php else : ?>
+	<?php else : ?>
 		<article id="post-0" class="post no-results not-found">
 			<header class="header">
 				<h1 class="entry-title"><?php esc_html_e( 'Nothing Found', 'cataco' ); ?></h1>
